fde9e79211cefda2479fa821cb1c6462a7973eba
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-04-23 22:15:00 -0300

    Updates

---
+++ precache-manifest.dfb9dc12c48169939911a1dcb3657a31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ee2a08c170b8dcc3a9c15191180f35",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "f87cf4afeec93ba57245",
+    "url": "/web/static/js/2.93970510.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web/static/js/2.93970510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cab324c3948e25dbdcc",
+    "url": "/web/static/js/main.a0469789.chunk.js"
+  },
+  {
+    "revision": "43be3371ee79e980b687",
+    "url": "/web/static/js/runtime-main.10b52e27.js"
+  },
+  {
+    "revision": "5ac0ce690eda9f45c3eeb08dbd98d673",
+    "url": "/web/static/media/background.5ac0ce69.jpg"
+  },
+  {
+    "revision": "de64cb1ea691888b664db2e6158a0410",
+    "url": "/web/static/media/be-the-hero.de64cb1e.png"
+  },
+  {
+    "revision": "77cc8976d4391258e4a43d95d8cefe53",
+    "url": "/web/static/media/me.77cc8976.jpeg"
+  },
+  {
+    "revision": "3dd2af2b843d8cf73e8263d278bd84ca",
+    "url": "/web/static/media/radar-dev.3dd2af2b.jpeg"
+  }
+]);
